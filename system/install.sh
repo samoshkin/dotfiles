aacf931bec196a9dec75165e9973131baf2e0528
Alexey Samoshkin <al.samoshkin@gmail.com>
2015-06-18 02:03:34 +0300

    Add system/install.sh. Install minimal set of fonts, packages and applications

--- system/install.sh
+++ system/install.sh
@@ -1,34 +1,27 @@
 #!/usr/bin/env bash
 
-log --err "Not implemented yet"
+log "Installing fonts"
 
-# install various packages
-# brew install
+brew tap caskroom/fonts
+brew cask install \
+	font-droid-sans-mono \
+	font-dejavu-sans \
+	font-inconsolata-dz \
+	font-source-code-pro || true
 
-# brew cask install
+log "Installing packages"
+brew install shellcheck todo-txt || true
 
-
-# Fonts evaluation for Atom
-#  \"SourceCodePro-Light\" [14-16] hard to focus, sharp but too thin
-#  \"SourceCodePro\" [14]
-#  \"Inconsolata-dz\" [14] stretched up
-#  \"FreeMono\" [15]
-#  \"DejaVu Sans Mono\"
-#  \"Droid Sans Mono\" [14]
-#  \"Input Mono,InputMono Light\"
-#  \"Input Mono\" [13-14]
-#  \"Input Mono,InputMono Light\" [13-14]
-#  \"Input Mono,InputMono Thin\" too thin
-#  \"Courier New\" [15]
-#  \"Courier\"
-#  \"Consolas \" [15]
-
-# view installed fonts
-# fc-list : family |  grep -i "source"  | uniq | sort
-
-# Install fonts from brew
-# brew tap caskroom/fonts
-# brew cask install font-droid-sans-mono font-freesans font-inconsolata-dz font-input font-source-code-pro %
-
-
-# https://github.com/miohtama/ztanesh/blob/master/zsh-scripts/rc/91-keybindings-osx
+log "Installing applications"
+brew cask install \
+	diffmerge \
+	google-chrome \
+	dropbox \
+	karabiner \
+	seil \
+	key-codes \
+	macpass \
+	skype \
+	slack \
+	utorrent \
+	vlc || true

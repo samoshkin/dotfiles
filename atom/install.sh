c7b3eaccbb7c8d5ad1a72dc6bdad1574e0399dfd
Alexey Samoshkin <al.samoshkin@gmail.com>
2015-06-18 02:01:26 +0300

    Remove not used font configuration from atom/config.cson. Install shellcheck along with atom installation

--- atom/install.sh
+++ atom/install.sh
@@ -4,6 +4,9 @@ log "Installing atom"
 
 brew cask install atom || true
 
+log "Installing shellcheck package"
+brew install shellcheck
+
 log "Installing atom packages"
 
 # install all packages
@@ -13,3 +16,18 @@ apm install --packages-file "${DOTFILES}/atom/packages.txt"
 log "Copy configuration to ~/.atom"
 # symlink config files to ~/.atom
 _ln -t ~/.atom ${DOTFILES}/atom/{init.coffee,keymap.cson,snippets.cson,styles.less,config.cson}
+
+# Fonts evaluation for Atom
+#  \"SourceCodePro-Light\" [14-16] hard to focus, sharp but too thin
+#  \"SourceCodePro\" [14]
+#  \"Inconsolata-dz\" [14] stretched up
+#  \"FreeMono\" [15]
+#  \"DejaVu Sans Mono\"
+#  \"Droid Sans Mono\" [14]
+#  \"Input Mono,InputMono Light\"
+#  \"Input Mono\" [13-14]
+#  \"Input Mono,InputMono Light\" [13-14]
+#  \"Input Mono,InputMono Thin\" too thin
+#  \"Courier New\" [15]
+#  \"Courier\"
+#  \"Consolas \" [15]

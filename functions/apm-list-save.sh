2411a79c023fa3fee3c197d1758aed419a3d57f2
Alexey Samoshkin <al.samoshkin@gmail.com>
2015-06-28 16:23:37 +0300

    Add functions to save apm packages list, iterm plist file. Add get-app-id to resolve app id from name

--- functions/apm-list-save.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-apm-list-save() {
-	apm list --installed --bare | sed 's/@.*//' | tee "${DOTFILES}/atom/packages.txt"
-}
--- functions/system.sh
+++ functions/system.sh
@@ -4,3 +4,22 @@ installed-fonts() {
 	local familyName=$1;
 	fc-list : family |  grep -i "$familyName"  | uniq | sort
 }
+
+get-app-id(){
+	osascript -e "id of app \"$1\""
+}
+
+save-config-atom-packages() {
+	local apmPackagesPath="${DOTFILES}/atom/packages.txt";
+	apm list --installed --bare | sed 's/@.*//' > "$apmPackagesPath"
+
+	echo -e "Saved to $apmPackagesPath"
+}
+
+save-config-iterm() {
+	local itermPlistPath="${DOTFILES}/iterm/com.googlecode.iterm2.plist"
+	defaults export com.googlecode.iterm2 "$itermPlistPath"
+	plutil -convert xml1 "$itermPlistPath"
+
+	echo -e "Saved to $itermPlistPath"
+}

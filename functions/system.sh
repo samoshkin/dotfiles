d53b1748e51cb7dbc8f3984b2c07abd97d032dc5
Alexey Samoshkin <al.samoshkin@gmail.com>
2015-07-12 00:53:01 +0300

    Common functions. Implement ask-question function to take user input (menu based or free form questions)

--- functions/system.sh
+++ functions/system.sh
@@ -126,6 +126,88 @@ battery-status(){
   pmset -g batt
 }
 
+# NOTE: works only in bash because uses IFS, zsh does not regard it
+ask-question(){
+  local choice
+  local defaultAnswer
+  local question
+  local prompt
+  local answer
+  local __resultVar="__answer"
+
+  # split choices with ;
+  local OIFS=$IFS
+  IFS=';'
+
+  # parse arguments
+  while [ "$1" != "" ]; do
+    case $1 in
+      --choice ) shift; choice=$1;;
+      --default ) shift; defaultAnswer=$1;;
+      --question ) shift; question=$1;;
+      * ) __resultVar=$1
+    esac
+    shift
+  done
+
+  prompt="$question: "
+
+  # choice based question
+  if [ -n "$choice" ]; then
+    if [ -n "$defaultAnswer" ]; then
+      prompt="$question [0=$defaultAnswer]: "
+    fi
+    printf "%s\n" "$prompt"
+
+    select answer in $choice;
+    do
+      answer=${answer:-$defaultAnswer}
+      echo $answer
+
+      # when answer is not blank we are done
+      if [ ! -z "$answer" ]; then
+        eval $__resultVar="'$answer'"
+        break
+      fi
+    done
+
+  # free form question
+  else
+    while true; do
+      if [ -n "$defaultAnswer" ]; then
+        prompt="$question [$defaultAnswer]: "
+      fi
+
+      printf "%s" "$prompt"
+      read answer
+      answer=${answer:-$defaultAnswer}
+
+      # when answer is blank and we do not have default value
+      if [ -z "$answer" ]; then
+        echo "Please provide an answer";
+      else
+        eval $__resultVar="'$answer'"
+        break
+      fi
+    done
+  fi
+
+  # restore back $IFS variable
+  IFS=$OIFS
+}
+
+generate-ssh-key(){
+  # TODO: ask for arguments interactively
+  # TODO: args: email, host alias(github.com), username(from email), keyname (user_at_github)
+  # TODO: make separate function to generate keys for any ssh connection, but w/o changing ssh-config
+  #
+  local keyname="${1:-$GIT_GITHUB_USER}"
+  local email="${2:-$GIT_USER_EMAIL}"
+  local keyfile="${HOME}/.ssh/${keyname}.key"
+
+  echo "Create key ${keyname} for user ${email} and store to ${keyfile}"
+}
+
 create-github-key(){
 
   # TODO: ask for arguments interactively

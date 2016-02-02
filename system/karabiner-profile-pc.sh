443d88208148152d905f92124dd6fa8146374428
Alexey Samoshkin <al.samoshkin@gmail.com>
2016-02-02 18:35:29 +0200

    Karabiner. Do not remap Ctrl+arrow to Option+arrow for PC keyboards

--- system/karabiner-profile-pc.sh
+++ system/karabiner-profile-pc.sh
@@ -4,15 +4,13 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set remap.pc_style_copy_paste 1
 /bin/echo -n .
-$cli set remap.save_winstyle_no_term 1
+$cli set remap.commandL2optionL 1
 /bin/echo -n .
 $cli set general.disable_numpad_hack 1
 /bin/echo -n .
 $cli set remap.control_delete_to_option_delete_winstyle 1
 /bin/echo -n .
 $cli set remap.redo_winstyle_no_term 1
-/bin/echo -n .
-$cli set remap.select_all_winstyle_no_term 1
 /bin/echo -n .
 $cli set remap.pointing_controlLeftclick2commandLeftclick 1
 /bin/echo -n .
@@ -30,9 +28,9 @@ $cli set remap.optionrcommandr 1
 /bin/echo -n .
 $cli set remap.pc_application2optionL 1
 /bin/echo -n .
-$cli set remap.pc_style_open 1
-/bin/echo -n .
 $cli set remap.pc_pause2eject 1
+/bin/echo -n .
+$cli set remap.pc_style_open 1
 /bin/echo -n .
 $cli set remap.undo_winstyle_no_term 1
 /bin/echo -n .
@@ -40,13 +38,13 @@ $cli set remap.pclikehomeend 1
 /bin/echo -n .
 $cli set remap.optionL2commandL 1
 /bin/echo -n .
+$cli set remap.save_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
 $cli set remap.shiftRshiftL_to_capslock 1
 /bin/echo -n .
 $cli set remap.copy_paste_winstyle_new_tab 1
-/bin/echo -n .
-$cli set remap.pclikecontrolarrow_except_vm 1
-/bin/echo -n .
-$cli set remap.commandL2optionL 1
 /bin/echo -n .
 $cli set repeat.initial_wait 300
 /bin/echo -n .

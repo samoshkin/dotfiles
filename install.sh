1a51c616098233be5e5a53e4b41b9959669a3368
Alexey Samoshkin <al.samoshkin@gmail.com>
2020-01-02 11:43:31 +0200

    Configure Karabiner key bindings

--- install.sh
+++ install.sh
@@ -10,15 +10,21 @@ mkdir -p "$DOTFILES/vendor"
 curl -sSL git.io/antigen > vendor/antigen.zsh
 
 # Symlink/copy configs to target directories
+# Git configs
 ln -sf "$DOTFILES/.gitconfig" "$HOME"
 ln -sf "$DOTFILES/.gitignore_global" "$HOME/.gitignore"
+
+# Shell and ZSH
 ln -sf "$DOTFILES/.inputrc" "$HOME"
 ln -sf "$DOTFILES/.zshenv" "$HOME"
 ln -sf "$DOTFILES/.zshrc" "$HOME"
-cp -f "$DOTFILES/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
+
+# Karabiner config
+mkdir -p "$HOME/.config/karabiner"
+cp -f "$DOTFILES/karabiner/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
 
 # Midnight commander
 mkdir -p ~/.config/mc
-ln -sf -t ~/.config/mc "${DOTFILES}/mc/ini" "${DOTFILES}/mc/menu" "${DOTFILES}/mc/mc.keymap"
+ln -sf -t ~/.config/mc "${DOTFILES}/mc/ini" "${DOTFILES}/mc/menu" "${DOTFILES}/mc/mc.keymapl"
 
 echo "Done"

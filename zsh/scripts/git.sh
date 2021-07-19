dd981950e38c73b9a20712175faa4fbea4874a93
Alexey Samoshkin <al.samoshkin@gmail.com>
2021-07-19 08:31:25 +0300

    Rework aliases and commands related to Git workflow

--- zsh/scripts/git.sh
+++ zsh/scripts/git.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
-# Git aliases and functions
-
-# git index (add, reset)
+# ==================================================
+# Preparing a commit
+# ==================================================
 alias giA='git add -A'
 alias gia="git add"
-alias gir="git reset"
-alias gid="git diff --cached"
+alias gir="git restore --staged"
 
-# git diff
-alias gd="git diff --stat -p"
-alias gds="git diff --stat"
-alias gdt='git difftool'
-alias ggdt='git difftool --gui'
-alias gmt='git mergetool'
+alias gwc="git clean -id"
+alias gwu="git restore --worktree"
+
+alias gcm="git commit"
+alias gce="git commit --amend --reuse-message HEAD"
+alias gcE="git commit --amend"
+
+# ==================================================
+# Status
+# ==================================================
+alias gws="git status --short"
+alias gwsi="git fuzzy status"
+
+# Git status in Vim using vim-fugitive plugin
+function gwsvim(){
+  vim -c ":Git"
+}
+
+# ==================================================
+# Diff
+# ==================================================
+alias gdd="git diff --stat -p"            # diff with stat and patch (via git-delta viewer)
+alias gds="git --no-pager diff --stat"              # diff with stat only
+alias gdt='git difftool'                 # difftool in Vim
+alias gdtui='git difftool --gui'          # DiffMerge or whatever GUI diff tool
 
 # git fuzzy diff, plus opening selected files in "git difftool"
-function gfd(){
+function gdi(){
   IFS=$'\n'
   files=($(git fuzzy diff "$@"))
 
@@ -25,18 +43,95 @@ function gfd(){
   fi
 }
 
-# git fuzzy diff for a single commit, similar to git show <commit>
-function gfdo() {
-  gfd "$1~" "$1"
+# ==================================================
+# Merging
+# ==================================================
+alias gmm='git merge --no-ff'
+alias gmt='git mergetool --no-ff'
+alias gmb="git show-branch --merge-base" # show commit merge-base between multiple refs
+alias gmc='git log --left-right --oneline --merge' # in case of conflicting merge, show which commits contribute to a conflict
+
+# ==================================================
+# Branching
+# ==================================================
+
+# list all branchs. Add "-a" to list both local and remote
+alias gbl="git branch -v"
+# find which branches contain given ref
+alias gbhas="git branch --contains"
+# show graph of branches, use --topics and --independent flags if needed
+alias gbg="git show-branch --date-order --sha1-name"
+
+# git checkout
+alias gbc="git checkout"
+# <git branch checkout interactive>
+alias gbci="git fuzzy branch"
+
+# Without arguments, shows all branch to choose from in fzf, with "git log" preview
+# With arguments, acts as a bare "git checkout"
+# <git branch checkout interactive x2>
+function gbcii() {
+  if [ "$#" -gt 0 ]; then
+    git checkout "$@"
+  else
+    local source_command="git branch -a"
+    local preview_command="git log --graph --decorate --date-order -n 50 --color=always --pretty=format:\"$_git_log_oneline_format\" {-1}"
+    local selection=$($source_command | fzf +m --ansi --preview-window="right:70%:nohidden:nowrap" --preview "$preview_command")
+
+    if [[ -n "$selection" ]]; then
+      git checkout "$(trim $selection)"
+    fi
+  fi
 }
 
-# git log
-alias glo='git log --topo-order --pretty=format:"$_git_log_oneline_format"'
-alias glg="glo --graph"
-alias gla="glo --graph --all"
+# git checkout where HEAD was 1 movement before (using 1st entry from reflog)
+# <git branch checkout back>
+function gbback(){
+  local head_prev_commit=$(git rev-parse HEAD@{1})
+  local head_prev_name=$(git name-rev "$head_prev_commit" | awk '{print $2}')
+  git checkout $head_prev_name
+}
 
-# Git file history
-function gh() {
+
+# ==================================================
+# Explore single commit
+# ==================================================
+
+# Show single commit description by given revision
+alias gcs="git --no-pager show -s --oneline"
+alias gcS="git --no-pager show -s"
+
+# git fuzzy diff for a single commit, similar to git show <commit>
+function gci() {
+  local rev=${1:-HEAD}
+  gdi "$rev~" "$rev"
+}
+
+
+# ==================================================
+# Log & history
+# ==================================================
+
+# git log
+alias glo='git log --date-order --pretty=format:"$_git_log_oneline_format"'
+alias glg="glo --date-order --graph"
+alias gla="glo --date-order --graph --all"
+
+# show left-right markers of triple dot revision syntax
+# $ gllr HEAD...MERGE_HEAD
+alias gllr="git log --oneline --graph --left-right --first-parent --boundary --decorate"
+
+# git log interactive. Opens selected commit in Vim via :Gedit command
+function gli() {
+  local commit=$(git fuzzy log "$@")
+
+  if [ -n "$commit" ]; then
+    vim -c ":Gedit $commit"
+  fi
+}
+
+# Git file history, shows only commits affecting given file. Open selected file from selected commit in Vim
+function glf() {
   if [ "$#" -eq 0 ]; then echo "Usage: gfh <file> <revision>"; return 1; fi
   if [[ ! -f "$1" ]]; then echo "Non existent file: $1"; return 1; fi
 
@@ -57,30 +152,24 @@ function gh() {
   fi
 }
 
-# git fuzzy log, plus opening selected commit in Vim via :Gedit command
-function gfl() {
-  local commit=$(git fuzzy log "$@")
+# ==================================================
+# Working with remotes
+# ==================================================
 
-  if [ -n "$commit" ]; then
-    vim -c ":Gedit $commit"
-  fi
-}
-
-# git pull and push
 alias gp="git push"
-alias gpc='git push --set-upstream origin "$(git-branch-current 2> /dev/null)"'
-alias gf="git fetch"
-alias gfm="git pull --rebase=false"
-alias gfr="git pull --rebase"
+alias gP='git push --set-upstream origin "$(git-branch-current 2> /dev/null)"'
+alias gpl="git pull"
 
-# git stash
-alias gss="git stash save --include-untracked"
-alias gs="git stash"
+# ==================================================
+# Stashing
+# ==================================================
+alias gss="git stash"
+alias gsS="git stash save --include-untracked"
 alias gsp="git stash pop"
-alias gsa="git stash apply"
 
-# Git stash list
-function gsl() {
+# Git stash interactive list
+# Apply selected stash
+function gsi() {
   local source_command="git stash list"
   local preview_command="echo {1} | awk -F: '{ print \$1 }' | xargs git --no-pager stash show"
   local selection=$($source_command | fzf +m --ansi --preview-window="right:70%:nohidden:nowrap" --preview "$preview_command")
@@ -90,57 +179,10 @@ function gsl() {
   fi
 }
 
-# Working tree
-alias gws="git status --short"
-alias gwc="git clean -id"
-alias gfs="git fuzzy status"
 
-alias gwU="git checkout HEAD --"
-alias gwu="git restore"
-
-alias grH="git reset --hard"
-
-# Git status in Vim using vim-fugitive plugin
-function gvs(){
-  vim -c ":Git"
-}
-
-# git commit
-alias gcm="git commit"
-alias gca="git commit --amend"
-alias gce="git commit --amend --reuse-message HEAD"
-
-# git checkout
-
-alias gfb="git fuzzy branch"
-
-# Without arguments, shows all branch to choose from in fzf, with "git log" preview
-# With arguments, acts as a bare "git checkout"
-function gco() {
-  if [ "$#" -gt 0 ]; then
-    git checkout "$@"
-  else
-    local source_command="git branch -a"
-    local preview_command="git log --graph --topo-order -n 50 --color=always --pretty=format:\"$_git_log_oneline_format\" {-1}"
-    local selection=$($source_command | fzf +m --ansi --preview-window="right:70%:nohidden:nowrap" --preview "$preview_command")
-
-    if [[ -n "$selection" ]]; then
-      git checkout "$selection"
-    fi
-  fi
-}
-
-# git checkout where HEAD was 1 movement before (using 1st entry from reflog)
-function gcop(){
-  local head_prev_commit=$(git rev-parse HEAD@{1})
-  local head_prev_name=$(git name-rev "$head_prev_commit" | awk '{print $2}')
-  git checkout $head_prev_name
-}
-
-# Open git object in a Vim (using :Gedit command)
-function ge() {
-  vim -c "Gedit $*"
-}
+# ==================================================
+# Pull requests
+# ==================================================
 
 # Review PR: show list of affected files, complete diff, and open selected file in Vim
 # NOTE: requires you to "git checkout <pr>" first
@@ -155,4 +197,13 @@ function git_review_pr(){
   if [ -n "$files" ]; then
     vim -c "let g:gitgutter_diff_base = '$REVIEW_UPSTREAM'" $files
   fi
+}
+
+# ==================================================
+# Misc
+# ==================================================
+
+# Open any git object in a Vim (using :Gedit command)
+function gitvim() {
+  vim -c "Gedit $*"
 }

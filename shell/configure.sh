8091548dd4f8e602db9675218127b775642e058a
Alexey Samoshkin <al.samoshkin@gmail.com>
2023-10-28 23:31:12 +0300

    Setup /etc/zshenv with ZDOTDIR override

--- shell/configure.sh
+++ shell/configure.sh
@@ -4,9 +4,9 @@ ZDOTDIR="$HOME/.config/zsh"
 SHELLENVDIR="$ZDOTDIR/.env"
 SHELLRCDIR="$ZDOTDIR/.rc"
 
-if ! grep -q 'ZDOTDIR' /etc/zshenv; then
-  cat >> "/etc/zshenv" << EOF
-  export ZDOTDIR="$HOME/.config/zsh"
+if [ ! -f /etc/zshenv ] || ! grep -q 'ZDOTDIR' /etc/zshenv; then
+  sudo tee -a "/etc/zshenv" >/dev/null << EOF
+export ZDOTDIR="$HOME/.config/zsh"
 EOF
 fi
 

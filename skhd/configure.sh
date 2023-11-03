daf99ea70d7908cd00680681aae953bc49c671df
Alexey Samoshkin <al.samoshkin@gmail.com>
2023-11-03 17:27:48 +0200

    Configure yabai and skhd

---
+++ skhd/configure.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+mkdir -p "$HOME/.config/skhd"
+ln -sf "$DOTFILES/skhd/skhdrc" "$HOME/.config/skhd/skhdrc"
---
+++ yabai/configure.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+mkdir -p "$HOME/.config/yabai"
+ln -sf "$DOTFILES/yabai/yabairc" "$HOME/.config/yabai/yabairc"

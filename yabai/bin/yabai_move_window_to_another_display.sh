07fcfa0d0d8ddce0cf13d5d158e288c07f9a3c98
Alexey Samoshkin <al.samoshkin@gmail.com>
2023-11-03 18:20:28 +0200

    yabai. fix script for moving window to another display

--- yabai/bin/yabai_move_window_to_another_display.sh
+++ yabai/bin/yabai_move_window_to_another_display.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
-
 cur_win=$(yabai -m query --windows --window | jq -r '.id')
 # since I have only 2 monitors, this essentially toggles window between displays
-yabai -m window --display next || yabai -m window --display first
+target_display=$( (yabai -m query --displays --display next || yabai -m query --displays --display first) | jq -r '.index')
 
-yabai_with_mouse_following_focus.sh yabai -m window --focus $cur_win;
-
+yabai -m window --display "$target_display"
+yabai_with_mouse_following_focus.sh yabai -m window --focus "$cur_win";

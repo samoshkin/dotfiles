39ce23ae26cc58d6d7484ce0c1f99d6dbaee4780
Alexey Samoshkin <al.samoshkin@gmail.com>
2023-11-05 20:01:24 +0200

    Change git configuration

--- git/configure.sh
+++ git/configure.sh
@@ -2,4 +2,4 @@
 
 ln -sf "$DOTFILES/git/.gitconfig" "$HOME"
 ln -sf "$DOTFILES/git/.gitignore_global" "$HOME/.gitignore"
-ln -sf -t "$SHELLRCDIR" "$DOTFILES/git/git.rc.sh"
+ln -sf "$DOTFILES/git/git.rc.sh" "$SHELLRCDIR"

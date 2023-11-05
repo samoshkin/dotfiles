52e16c433bc1b4144c6478bf74f8b7dc6e72e69b
Alexey Samoshkin <oleksii.samoshkin@devnull.local>
2023-11-05 19:52:50 +0200

    Fix ssh configuration

--- ssh/configure.sh
+++ ssh/configure.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p "$HOME/.config/karabiner"
+mkdir -p "$HOME/.ssh"
 
 if [ ! -f "$HOME/.ssh/config" ]; then
   cp "$DOTFILES/ssh/config" "$HOME/.ssh/config"
 fi
 
-ln -sf -t "$SHELLRCDIR" "$DOTFILES/ssh/ssh.rc.sh"
+ln -sf "$DOTFILES/ssh/ssh.rc.sh" "$SHELLRCDIR"

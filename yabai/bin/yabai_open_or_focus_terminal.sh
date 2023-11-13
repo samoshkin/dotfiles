dce87cda05a669934e4465b447f11cca78fe3a6a
Alexey Samoshkin <al.samoshkin@gmail.com>
2023-11-13 22:35:11 +0200

    yabai. tweak configuration for always on top terminal

--- yabai/bin/yabai_open_or_focus_terminal.sh
+++ yabai/bin/yabai_open_or_focus_terminal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-iterm_window_id=$(yabai -m query --windows --space | jq '[.[] | select(.app == "iTerm2")] | last | .id | select(.!=null)')
+iterm_window_id=$(yabai -m query --windows --space | jq '[.[] | select(.app == "iTerm2" and .title != "sticky")] | last | .id | select(.!=null)')
 
 if [ -n "$iterm_window_id" ]; then
   yabai -m window --focus $iterm_window_id
--- yabai/bin/yabai_open_side_terminal.sh
+++ yabai/bin/yabai_open_side_terminal.sh
@@ -2,8 +2,6 @@
 
 iterm_sticky_window_id=$(yabai -m query --windows --space | jq 'map(select(.app == "iTerm2" and (.title | test("^sticky")))) | .[0] | select(. != null) | .id')
 
-echo $iterm_sticky_window_id
-
 if [ -z "$iterm_sticky_window_id" ]; then
   window_id=$(osascript << EOD
 tell application "iTerm"
@@ -11,12 +9,14 @@ tell application "iTerm"
   tell newWindow to return id
 end tell
 EOD
+  yabai -m window $window_id --toggle float --grid 2:2:0:0:2:1 --layer normal
+  yabai -m window --focus $window_id
 )
-  echo $window_id
 else
   is_minimized=$(yabai -m query --windows --window $iterm_sticky_window_id | jq '."is-minimized"')
   if [ "$is_minimized" = "true" ]; then
     yabai -m window --deminimize $iterm_sticky_window_id
+    yabai -m window --focus $window_id
   else
     yabai -m window --minimize $iterm_sticky_window_id
   fi
